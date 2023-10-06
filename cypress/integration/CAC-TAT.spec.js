a7d5d161be30e8284020d9aa8009803d5b376d08
Manoel Tindao <timdao02@hotmail.com>
2023-10-06 07:32:20 -0300

    Aula 06

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -134,12 +134,57 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .should('have.value', 'feedback')
   })
 
-  it.only('marca cada tipo de atendimento', () => {
+  it('marca cada tipo de atendimento', () => {
     cy.get('input[type="radio"]')
       .should('have.length', 3)
       .each($radio => {
         cy.wrap($radio).check()
         cy.wrap($radio).should('be.checked')
+      })
+  })
+
+  it('uncheck',() => {
+    cy.get('#email-checkbox')
+      .not('.checked')
+      .check()
+      .should('be.checked')
+      .uncheck()
+      .not('.checked')
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#phone-checkbox')
+      .check()
+    cy.fillMandatoryFieldsAndSubmit()
+    cy.get('.error').should('be.visible')
+    cy.get('.error > strong').should('have.text', 'Valide os campos obrigatórios!')
+  })
+
+  it('Seleciona um arquivo da pasta fixtures', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json')
+      .should($input => {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+      .should($input => {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('@sampleFile', {action: 'drag-drop'})
+      .should($input => {
+        expect($input[0].files[0].name).to.equal('example.json')
       })
   })
 
